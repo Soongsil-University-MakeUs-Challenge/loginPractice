5712aeea883cc11577763eba09706839cf2e42d6
11t518s <11t518s20161759@gmail.com>
2022-05-24 11:21:44 +0900

    remove useless module

--- server/src/router/user/user.repository.ts
+++ server/src/router/user/user.repository.ts
@@ -3,7 +3,6 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { UserSignUpBodyDto } from './dto/userSignUpBodyDto';
 import { User } from '../../schema/user.schema';
-import { EphemeralKeyInfo } from 'tls';
 
 @Injectable()
 export class UserRepository {
