9bfcf199193545b4305730a6ada594afaa0c8b31
seyxxn <lucy9480@gmail.com>
2022-05-28 17:55:01 +0900

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -4,47 +4,29 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>teasdfasdfsdfasdfst</div>
+        <div>login-practice</div>
       </header>
+      <body>
+        <p>Sign in</p>
+        <form action="" method="POST">
+          <div class="input-box">
+            <input id="username" type="text" name="username" placeholder="ID" />
+          </div>
+
+          <div class="input-box">
+            <input
+              id="password"
+              type="password"
+              name="password"
+              placeholder="PW"
+            />
+          </div>
+          <input type="button" value="LOG IN" />
+        </form>
+        <input type="button" value="CREATE ACCOUNT" />
+      </body>
     </div>
   );
 }
 
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
